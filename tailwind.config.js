e10f74abfc565c9fb6bd5a33ac465d3e41277d07
ehroy <anjaykoko69@gmail.com>
2024-06-11 14:12:08 +0700

    add-breze

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.jsx",
-        "./resources/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
     ],
-    darkMode: false,
+
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    plugins: [require("@tailwindcss/forms")],
+
+    plugins: [forms],
 };
